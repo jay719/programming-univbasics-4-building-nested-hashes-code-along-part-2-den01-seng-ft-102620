23d31678205ea1dfb861ae6adf325500df3a99a9
Javaria Brascom <javariab17@gmail.com>
2020-09-22 04:05:48 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -50,7 +50,7 @@ def add_characters
          :heroine => {name: "Juliet", age: "15", status: "alive"},
          :heroine_friends => [
          {name: "Steven", age: "30", attitude: "confused"},
-         {name: "Nurse", age: "44", attitude: "worried"}
+         S
          ]
       }
    }
