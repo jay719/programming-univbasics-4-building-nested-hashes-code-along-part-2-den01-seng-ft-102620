e20001b4e2d5f9c0e7f2e41ed24f6e062c7d42e6
Javaria Brascom <javariab17@gmail.com>
2020-09-22 04:04:41 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -57,6 +57,7 @@ def add_characters
    epic_tragedy[:additional_characters] = [
   { name: "Prince Escalus" },
   { name: "Apothecary" }
+  }
 
    # Write code below that does the following programmatically:
    #
